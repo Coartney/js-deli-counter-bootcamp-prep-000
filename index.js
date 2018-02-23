45bb0aa803655c456842c3b3211f5ec484932d63
coartney22@gmail.com <coartney22@gmail.com>
2018-02-23 01:30:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ function currentLine(katzDeliLine){
     newLine.push(` ${n}. ${customer}`)
     i++;
     n++;
-  }return `The line is currently:${newLine}`
-  }  
+  } return `The line is currently:${newLine}`
+  }  else {
   return 'The line is currently empty.'
+  }
 }
